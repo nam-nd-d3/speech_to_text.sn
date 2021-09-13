3065a160892f5ed76cf31501232dbe86d4c58025
unknown <kingkcode@gmail.com>
2021-09-13 09:15:05 +0700

    OK

--- core/split_dataset.py
+++ core/split_dataset.py
@@ -1,45 +1,36 @@
 import os
 import pandas as pd
-import logging
 import argparse
 
-logger = logging.getLogger(__name__)
+parser = argparse.ArgumentParser(description='Split datasets wav2vec2.')
+parser.add_argument('--root_data_csv',
+                    type=str,
+                    required=True,
+                    help="str - csv directory path : type (path_file, transcripts)")
 
+parser.add_argument('--train_data_csv',
+                    type=str,
+                    required=True,
+                    help="str - csv directory path : type (path_file, transcripts)")
 
-def main():
-    parser = argparse.ArgumentParser(description='Split datasets wav2vec2.')
-    parser.add_argument('--root_data_csv',
-                        type=str,
-                        required=True,
-                        help="str - csv directory path : type (path_file, transcripts)")
+parser.add_argument('--test_data_csv',
+                    type=str,
+                    required=True,
+                    help="str - csv directory path : type (path_file, transcripts)")
 
-    parser.add_argument('--train_data_csv',
-                        type=str,
-                        required=True,
-                        help="str - csv directory path : type (path_file, transcripts)")
+parser.add_argument('--ratio_split',
+                    default=0.8,
+                    type=float,
+                    required=True,
+                    help="str - ratio to split dataset")
 
-    parser.add_argument('--test_data_csv',
-                        type=str,
-                        required=True,
-                        help="str - csv directory path : type (path_file, transcripts)")
+args = parser.parse_args()
 
-    parser.add_argument('--ratio_split',
-                        default=0.8,
-                        type=float,
-                        required=True,
-                        help="str - ratio to split dataset")
+root_dataset = pd.read_csv(args.root_data_csv)
 
-    args = parser.parse_args()
-
-    root_dataset = pd.read_csv(args.root_data_csv)
-
-    msk = np.random.rand(len(root_dataset)) <= args.ratio_split
-    train_csv = root_dataset[msk]
-    test_csv = root_dataset[~msk]
-    train_csv.to_csv(args.train_data_csv)
-    test_csv.to_csv(args.test_data_csv)
-    print(f"Train size: {len(train_csv)} - Test size: {len(test_csv)}")
-
-
-if __name__ == "__main___":
-    main()
+msk = np.random.rand(len(root_dataset)) <= args.ratio_split
+train_csv = root_dataset[msk]
+test_csv = root_dataset[~msk]
+train_csv.to_csv(args.train_data_csv)
+test_csv.to_csv(args.test_data_csv)
+print(f"Train size: {len(train_csv)} - Test size: {len(test_csv)}")
