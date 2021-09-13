8aa55f711b9c8405604084f9f883852c737d60f3
unknown <kingkcode@gmail.com>
2021-09-13 09:11:50 +0700

    OK

--- core/split_dataset.py
+++ core/split_dataset.py
@@ -2,9 +2,8 @@ import os
 import pandas as pd
 import logging
 
-from pathlib import Path
-
 logger = logging.getLogger(__name__)
+
 
 def main():
     parser = argparse.ArgumentParser(description='Split datasets wav2vec2.')
@@ -12,33 +11,34 @@ def main():
                         type=str,
                         required=True,
                         help="str - csv directory path : type (path_file, transcripts)")
-                        
+
     parser.add_argument('--train_data_csv',
                         type=str,
                         required=True,
                         help="str - csv directory path : type (path_file, transcripts)")
-                        
+
     parser.add_argument('--test_data_csv',
                         type=str,
                         required=True,
                         help="str - csv directory path : type (path_file, transcripts)")
-                  
+
     parser.add_argument('--ratio_split',
                         default=0.8,
                         type=float,
                         required=True,
                         help="str - ratio to split dataset")
-                        
+
     args = parser.parse_args()
-    
+
     root_dataset = pd.read_csv(args.root_data_csv)
-    
+
     msk = np.random.rand(len(root_dataset)) <= args.ratio_split
     train_csv = root_dataset[msk]
     test_csv = root_dataset[~msk]
     train_csv.to_csv(args.train_data_csv)
     test_csv.to_csv(args.test_data_csv)
-    logger.info(f"Train size: {len(train_csv)} - Test size: {len(test_csv)}")  
+    print(f"Train size: {len(train_csv)} - Test size: {len(test_csv)}")
 
-if __name__=="__main___":
+
+if __name__ == "__main___":
     main()
